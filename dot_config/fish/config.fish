7359d9724b96fc540d7fad4a230e9b7ec3b7d47a
Benjamin Fovet <b.fovet@gmail.com>
2025-09-28 22:12:08 +0200

    run zellij when fish starts

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,13 +1,22 @@
 if status is-interactive
-   set -U fish_greeting
-   starship init fish | source
+    set -U fish_greeting
+    starship init fish | source
 
-   fish_add_path $HOME/.local/bin
-   fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.cargo/bin
 
-   zoxide init fish | source
-   alias cd="z"
-   alias ls="eza --icons"
-   alias ll="eza -l --icons"
-   alias cat="batcat --paging=never"
+    zoxide init fish | source
+    alias cd="z"
+    alias ls="eza --icons"
+    alias ll="eza -l --icons"
+    alias cat="bat --paging=never"
+
+    # At this point, specify the Zellij config dir, so we can launch it manually if we want to
+    export ZELLIJ_CONFIG_DIR=$HOME/.config/zellij
+
+    # Check if our Terminal emulator is Ghostty
+    if [ "$TERM" = xterm-ghostty ]
+        # Launch zellij
+        eval (zellij setup --generate-auto-start fish | string collect)
+    end
 end
