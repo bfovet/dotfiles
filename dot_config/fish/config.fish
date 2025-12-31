f95b1c7c3bdd21858219452b25ad630b30aee186
Benjamin Fovet <b.fovet@gmail.com>
2025-12-31 14:32:16 +0100

    feat: activate mise in current shell

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -19,4 +19,10 @@ if status is-interactive
         # Launch zellij
         eval (zellij setup --generate-auto-start fish | string collect)
     end
+
+    if status is-interactive
+        mise activate fish | source
+    else
+        mise activate fish --shims | source
+    end
 end
